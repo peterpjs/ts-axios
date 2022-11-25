2bd3bc4c3dcf8c49c59e72a5768d2567055e9fc5
peterpjs <243153073@qq.com>
2022-11-25 17:08:04 +0800

    update code

--- src/index.ts
+++ src/index.ts
@@ -1,17 +1,38 @@
-import { AxiosRequestConfig } from './types'
+import { AxiosRequestConfig, AxiosPromise, AxiosResponse } from './types'
 import xhr from './xhr'
 import { bulidURL } from './helpers/url'
+import { transformRequest, transformResponse } from './helpers/data'
+import { processHeaders } from './helpers/header'
 
-function axios(config: AxiosRequestConfig) {
+function axios(config: AxiosRequestConfig): AxiosPromise {
   processConfig(config)
-  xhr(config)
+  return xhr(config).then(res => {
+    return transformResponseData(res)
+  })
 }
 function processConfig(config: AxiosRequestConfig): void {
   config.url = transformURL(config)
+  config.headers = transformHeaders(config)
+  config.data = transformRequestData(config)
 }
 
 function transformURL(config: AxiosRequestConfig): string {
   const { url, params } = config
   return bulidURL(url, params)
 }
+
+function transformRequestData(config: AxiosRequestConfig): any {
+  return transformRequest(config.data)
+}
+
+function transformHeaders(config: AxiosRequestConfig): any {
+  const { headers = {}, data } = config
+  return processHeaders(headers, data)
+}
+
+function transformResponseData(res: AxiosResponse): AxiosResponse {
+  res.data = transformResponse(res.data)
+  return res
+}
+
 export default axios
